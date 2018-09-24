bbdb60299580f5231a085ebfd1b6fae8fd8cc96a
stefans2708 <stefan.s27.08@gmail.com>
2018-09-25 00:09:56 +0200

    showing current location on map for adding post

--- Manifesto/app/src/main/java/com/example/stefan/manifesto/ui/activity/AddPostActivity.java
+++ Manifesto/app/src/main/java/com/example/stefan/manifesto/ui/activity/AddPostActivity.java
@@ -1,10 +1,19 @@
 package com.example.stefan.manifesto.ui.activity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.widget.Toolbar;
 
 import com.example.stefan.manifesto.MapActivity;
 import com.example.stefan.manifesto.R;
@@ -17,7 +26,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class AddPostActivity extends BaseActivity implements OnMapReadyCallback{
+public class AddPostActivity extends BaseActivity implements OnMapReadyCallback {
+
+    private static final int PERMISSION_ACCESS_FINE_LOCATION = 1;
 
     private AddPostViewModel viewModel;
     private ActivityAddPostBinding binding;
@@ -31,7 +42,7 @@ public class AddPostActivity extends BaseActivity implements OnMapReadyCallback{
         viewModel = ViewModelProviders.of(this).get(AddPostViewModel.class);
         binding.setViewModel(viewModel);
 
-        initMap();
+        initViews();
         setUpObservers();
     }
 
@@ -45,7 +56,14 @@ public class AddPostActivity extends BaseActivity implements OnMapReadyCallback{
 
     }
 
-    private void initMap() {
+    private void initViews() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        setTitle("Add post");
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
         mapFragment.getMapAsync(this);
     }
@@ -54,8 +72,32 @@ public class AddPostActivity extends BaseActivity implements OnMapReadyCallback{
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
 
-        LatLng sydney = new LatLng(-34, 151);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_ACCESS_FINE_LOCATION);
+        } else {
+            setMyLocationMarker();
+        }
+    }
+
+    private void setMyLocationMarker() {
+        LocationManager locationManager = ((LocationManager) getSystemService(Context.LOCATION_SERVICE));
+        @SuppressLint("MissingPermission") Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        googleMap.addMarker(new MarkerOptions().position(myLocation).title("My current position"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_ACCESS_FINE_LOCATION:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    setMyLocationMarker();
+                }
+                return;
+            }
+        }
     }
 }
