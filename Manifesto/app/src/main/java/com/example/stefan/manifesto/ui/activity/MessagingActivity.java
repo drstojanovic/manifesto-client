2ad4b785714aea5b1962b85d4e660d4236d492a7
stefans2708 <stefan.s27.08@gmail.com>
2018-10-02 22:57:14 +0200

    messaging - created layouts

---
+++ Manifesto/app/src/main/java/com/example/stefan/manifesto/ui/activity/MessagingActivity.java
@@ -0,0 +1,15 @@
+package com.example.stefan.manifesto.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MessagingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+}
